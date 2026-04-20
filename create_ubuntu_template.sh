93fc865b29f757f65643bd4a0b8bcd69efc3ee0e
AVSsystems <avs.systems.ttu@gmail.com>
2026-04-20 17:11:45 -0500

    Create create_ubuntu_template.sh

---
+++ create_ubuntu_template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Automate Ubuntu 22.04 Cloud-Init Image for AVS Labs
+wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+qm create 9000 --name "ubuntu-2204-template" --memory 2048 --net0 virtio,bridge=vmbr0
+qm importdisk 9000 jammy-server-cloudimg-amd64.img local-lvm
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
+qm set 9000 --boot c --bootdisk scsi0
+echo "Template 9000 Created."
